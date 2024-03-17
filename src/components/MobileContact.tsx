d77357c13e9ce76ef762031fcf24910b442e791e
Gerald Encabo <geraldencabo@gmail.com>
2024-03-17 16:19:44 -0400

    Update

--- src/components/MobileContact.tsx
+++ src/components/MobileContact.tsx
@@ -3,6 +3,8 @@ import { NavLink } from 'react-router-dom';
 import { GoMail } from "react-icons/go";
 import { MdLocalPhone } from "react-icons/md";
 import { useEffect, useState, useRef  } from 'react';
+import { FaLocationDot } from "react-icons/fa6";
+import { ImArrowUp } from "react-icons/im";
 
 const MobileContact = ({scrollContact}: any) => {
 
@@ -20,24 +22,43 @@ const MobileContact = ({scrollContact}: any) => {
       })
     }, [contactFooterBtn])
 
+    const scrollUp = () => {
+        window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+        })
+    }
+
     return (
         <section className="mobile-contact" ref={navbarRef}>
             {
                 contactFooterBtn && (
                     <>
-                        <NavLink                   
-                            to="tel:204-942-7577"
-                            id="mobile-contact-phone"
-                        >
-                            <MdLocalPhone />
-                        </NavLink>
-                        <NavLink 
-                            to={''} 
-                            onClick={scrollContact}
-                            id="mobile-contact-mail"
-                        >
-                            <GoMail />
-                        </NavLink>
+                        <div className='mobile-contact-btn'>
+                            <NavLink                   
+                                to="tel:204-942-7577"
+                                id="mobile-contact-phone"
+                                >
+                                <MdLocalPhone />
+                            </NavLink>
+                            <NavLink 
+                                to={''} 
+                                onClick={scrollContact}
+                                id="mobile-contact-mail"
+                                >
+                                <GoMail />
+                            </NavLink>
+                            <NavLink
+                                to={''}
+                                onClick={scrollUp}
+                                id="mobile-scroll-btn"
+                            >
+                                <ImArrowUp />
+                            </NavLink>
+                        </div>
+                        <p className='mobile-contact-address'>        
+                            <span><FaLocationDot /></span> 402-428 Portage Avenue, Winnipeg, MB R3C 0E2 CA
+                        </p>
                     </>
                 )
             }
